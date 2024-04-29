f39f6f8d81fd6f46db4d2a275e50306d797a5b07
Jaclyn Sunardi <jaclyn.sunardi@yahoo.ca>
2024-04-29 13:24:57 -0700

    linked to signup page

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
  
 app = Flask(__name__)
-
-app.config['DEBUG'] = True
  
 @app.route('/')
 
 def index():
     return render_template('index.html')
- 
- 
+
+@app.route('/signup', methods=['GET'])
+
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('signup').addEventListener('click', function() {
+        window.location.href = '/signup';
+    });
+});
